a2ab342fb741396027f9b26931bd0c9cdbcc312d
Abhay Sharma <shabhay007@gmail.com>
2023-04-01 00:41:02 +0530

    second changes

---
+++ build/precache-manifest.4ee5fa080ff617a4f4783364497e3ea1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e047219f15f0b01dbf0ebad5e75797c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7670ab1cefdcaf9a14ec",
+    "url": "/static/css/main.a67b46ed.chunk.css"
+  },
+  {
+    "revision": "f61ee3ed882edcda979a",
+    "url": "/static/js/2.3fb59f59.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3fb59f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7670ab1cefdcaf9a14ec",
+    "url": "/static/js/main.65747a88.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/static/media/bg.8e5b87b6.webp"
+  }
+]);
